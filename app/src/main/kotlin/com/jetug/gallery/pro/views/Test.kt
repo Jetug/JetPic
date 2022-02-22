1cce991f4676d3bde6c9ccfb25516649ce542e5c
Jetug <ksergeev01@gmail.com>
2022-02-22 14:56:03 +0300

    22.02.2022

---
+++ app/src/main/kotlin/com/jetug/gallery/pro/views/Test.kt
@@ -0,0 +1,9 @@
+package com.jetug.gallery.pro.views
+
+import android.content.Context
+import android.util.AttributeSet
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
+
+class Test @JvmOverloads constructor(context: Context, attr: AttributeSet? = null) : SubsamplingScaleImageView(context, attr) {
+
+}
